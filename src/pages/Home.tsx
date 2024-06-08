7605070f06d028de30d9948cafd960806d6ea8b4
MelSkywalker <darky.tkd@gmail.com>
2024-06-08 15:37:16 -0600

    Add pokemon title to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
 import PokemonList from "../components/PokemonList";
-import './home.scss';
+import pokemonTitle from "../assets/images/pokemon-title.jpg";
+import "./home.scss";
 
 function Home () {
     return (
         <div className="home-page">
-            <div className="head" />
+            <div className="head">
+                <img src={pokemonTitle} alt="pokemon title" />
+            </div>
             <PokemonList />
         </div>
     )
